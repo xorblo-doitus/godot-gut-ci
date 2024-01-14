5d544f7be5b11bfd4dfed2c0cdf0c9ebe14358d1
xorblo-doitus <75997617+xorblo-doitus@users.noreply.github.com>
2024-01-14 15:06:52 +0100

    Fix order

--- init-and-run-tests.sh
+++ init-and-run-tests.sh
@@ -42,8 +42,8 @@ if [ $is_version_4 == "true" ] && ! test -f ./.godot/global_script_class_cache.c
   FILE_COUNT=$(find ./.godot -type f | wc -l)
   while [[ ! ( (-f "./.godot/global_script_class_cache.cfg") && ($FILE_COUNT==$(find ./.godot -type f | wc -l)) ) ]]; do
     echo $FILE_COUNT;
-    sleep 2s;
     FILE_COUNT=$(find ./.godot -type f | wc -l);
+    sleep 2s;
   done
   sleep 0.1s
   kill $godotpid
